c8374179c812838d15e3749a40cd5920eaf04d31
NewP1 <jaehyuk712@gmail.com>
2023-01-22 17:29:56 +0900

    Dynamic Programming

---
+++ Algorithm/Dynamic Programming/Baekjoon_2193.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = [0] * 91
+arr[1] = 1
+if n == 1:
+    print(arr[1])
+else:
+    for i in range(2, n+1):
+        arr[i] = arr[i-1] + arr[i-2]
+    print(arr[n])
