19e427c837b72ebcf6e5b5f8651e2c7b432522bf
NewP1 <jaehyuk712@gmail.com>
2023-01-24 16:13:03 +0900

    Dynamic Programming

---
+++ Algorithm/Dynamic Programming/Baekjoon_9095.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+arr = [0] * 11
+arr[1] = 1
+arr[2] = 2
+arr[3] = 4
+for i in range(4, 11):
+    arr[i] = arr[i-3] + arr[i-2] + arr[i-1]
+for _ in range(t):
+    print(arr[int(input())])
